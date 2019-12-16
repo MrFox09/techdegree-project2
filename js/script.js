ef51d72f6c5c4893c10b49bb813373eb3709c866
Christian Fuchs <58251727+MrFox09@users.noreply.github.com>
2019-12-16 20:20:08 +0100

    Update script.js
    added more comments

--- js/script.js
+++ js/script.js
@@ -8,14 +8,8 @@ FSJS project 2 - List Filter and Pagination
 
 
 /***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
+   set Global variables to store the listItem which will be manipulated
+   set the number of shown listItems(students) per page
 ***/
 const listItems = document.getElementsByClassName('student-item cf');
 const shownStudent = 10;
